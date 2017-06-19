d7d07c7f609c215c5288a97c2fbbcb98cfddcf8c
Victor Trac <victor@cloudkite.io>
2017-06-19 11:59:56 -0600

    add cloudsql to example gke scopes

--- example_templates/gke_cluster.py
+++ example_templates/gke_cluster.py
@@ -48,7 +48,7 @@ class GKECluster(Template):
                         httpLoadBalancing=HttpLoadBalancingProperty(disabled=False)
                     ),
                     description='{} {} GKE Cluster'.format(self.env, cluster.get('name')),
-                    #initialClusterVersion='1.6.2',
+                    initialClusterVersion='1.6.4',
                     network='{}-network'.format(self.env),
                     nodePools=[
                         NodePoolProperty(
@@ -63,21 +63,22 @@ class GKECluster(Template):
                                 diskSizeGb=nodepool.get('disk_size'),
                                 # see https://developers.google.com/identity/protocols/googlescopes
                                 oauthScopes=[
-                                    "https://www.googleapis.com/auth/userinfo.email",
-                                    "https://www.googleapis.com/auth/compute",
-                                    "https://www.googleapis.com/auth/devstorage.read_write",
-                                    "https://www.googleapis.com/auth/taskqueue",
                                     "https://www.googleapis.com/auth/bigquery",
+                                    "https://www.googleapis.com/auth/bigtable.data",
+                                    "https://www.googleapis.com/auth/compute",
+                                    "https://www.googleapis.com/auth/cloud-platform",
                                     "https://www.googleapis.com/auth/datastore",
+                                    "https://www.googleapis.com/auth/devstorage.read_write",
                                     "https://www.googleapis.com/auth/logging.write",
                                     "https://www.googleapis.com/auth/monitoring",
-                                    "https://www.googleapis.com/auth/cloud-platform",
-                                    "https://www.googleapis.com/auth/bigtable.data",
                                     "https://www.googleapis.com/auth/pubsub",
                                     "https://www.googleapis.com/auth/servicecontrol",
                                     "https://www.googleapis.com/auth/service.management",
+                                    "https://www.googleapis.com/auth/sqlservice.admin",
+                                    "https://www.googleapis.com/auth/source.read_write",
+                                    "https://www.googleapis.com/auth/taskqueue",
                                     "https://www.googleapis.com/auth/trace.append",
-                                    "https://www.googleapis.com/auth/source.read_write"
+                                    "https://www.googleapis.com/auth/userinfo.email",
                                 ]
                             ),
                             initialNodeCount=nodepool.get('node_count'),
